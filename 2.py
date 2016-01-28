07c2e4f84840c063872e95886b68d373c80db1f7
liuxkay <sccck000@gmail.com>
2016-01-28 19:27:07 +0900

    2.py added

---
+++ 2.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin/python
+import socket
+UDP_IP = "10.8.0.6"
+UDP_PORT = 1194
+MESSAGE = "disable that"
+
+print "UDP target IP:", UDP_IP
+print "UDP target port:", UDP_PORT
+print "message:", MESSAGE
+
+sock = socket.socket(socket.AF_INET, # Internet
+                 socket.SOCK_DGRAM) # UDP
+sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
--- pandas.py
+++ pandas.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env/python
 ## objective to scrape table from a website and save it as csv file
 ##reference http://qiita.com/kitsuyui/items/4906bb457af4d0e2d0a5
-import pandas
-url = 'http://typhoon.yahoo.co.jp/weather/jp/earthquake/list/?sort=1&key=1&b=15001'
-fetched_dataframes = pandas.io.html.read_html(url)
-fetched_dataframes[0]
-fetched_dataframes[0].to_csv('apollo.csv')
+# -*- coding: utf-8 -*-
+import pandas as pd
+urlString = "http://typhoon.yahoo.co.jp/weather/jp/earthquake/list/?sort=1&key=1&b=15001"
+import lxml.html as LH
+result = pd.io.LH.read_html(urlString)
+
+print (result)
