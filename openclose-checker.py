0e6b82df18c83a4c8e1c1e0db04c12d89f57ecfa
liuxkay <sccck000@gmail.com>
2016-12-14 16:27:20 +0900

    added expression in openclose-checker.py

--- openclose-checker.py
+++ openclose-checker.py
@@ -4,14 +4,14 @@
 def yes_no_input():
     while True:
         choice = raw_input("This will read open or close information 'Type anything': ").lower()
-        if choice in ['運行', '全面滑走可能', '平常運転','○','yes','ya']:
+        if choice in ['運行', '全面滑走可能', '平常運転','○','● ','open','OPEN','Open','◎','◯','一部滑走可能']:
             return True
-        elif choice in ['close','CLOSE','－','No','-']:
+        elif choice in ['運休','時間外','運転見合わせ','close','CLOSE','Close','－','×','-','営業終了','ー']:
             return False
 
 
 if __name__ == '__main__':
     if yes_no_input():
-        print('運営中')
+        print('リフト運行中')
     else:
         print('運休')
